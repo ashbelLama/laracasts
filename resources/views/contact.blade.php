40ba2c9dbf6f2d5b72299e2cbb14a2e9ff3255d0
axl_ashbel <ashbel@aximpro.io>
2024-09-17 13:43:58 +0545

    Added support for tailwindcss

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+  <x-slot:heading>
+    Contact Page
+  </x-slot:heading>
+  <h3>Contact Page</h3>
+  <p>
+    Email: contact_us@test.com <br>
+    Contact no: +01 5555478
+  </p>
+</x-layout>
